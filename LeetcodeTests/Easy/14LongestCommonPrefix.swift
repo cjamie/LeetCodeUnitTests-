22bc27f3308abf3be6cdac377b20ddc45965495f
Jamie Chu <jamie.chu@nike.com>
2020-12-31 22:25:52 -0500

    added Trie algorithm for LongestCommonPrefix

--- LeetcodeTests/Easy/14LongestCommonPrefix.swift
+++ LeetcodeTests/Easy/14LongestCommonPrefix.swift
@@ -41,27 +41,96 @@ class LongestCommonPrefix: XCTestCase {
 
     func test() {
         let input = ["flower","flow","flight"]
-        
+
         let actual = longestCommonPrefix(input)
-        
-        
+
+
         XCTAssertEqual(actual, "fl")
         
     }
     
-    func longestCommonPrefix(_ strs: [String]) -> String {
+    // This is normal way of doing this
+    func longestCommonPrefix1(_ strs: [String]) -> String {
         guard let firstEntry = strs.first else { return "" }
         var prefixCounter = firstEntry
-                
+
         for s in strs {
             let newPrefix = s.commonPrefix(with: prefixCounter)
-            if newPrefix.isEmpty {
+            if  newPrefix.isEmpty {
                 return newPrefix
             } else {
                 prefixCounter = newPrefix
             }
         }
-        
+
         return prefixCounter
     }
+
+    // this will use a trie
+    // TODO: - resolve edge cases
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        let trie = WordTrie()
+        trie.insert(words: strs)
+        return trie.longestPrefixWord()
+    }
+}
+
+private class TrieNode<T: Hashable> {
+    // NOTE: - only the sentinel will have a nil value
+    let value: T?
+    private(set) var children: [T: TrieNode<T>] = [:]
+    var isEnd = false
+
+    init(value: T? = nil) {
+        self.value = value
+    }
+    
+    func addChild(withValue value: T) {
+        guard children[value] == nil else { return }
+        children[value] = TrieNode(value: value)
+    }
+}
+
+
+private class WordTrie {
+    typealias Node = TrieNode<Character>
+    private let sentinel = Node()
+    
+    func insert(word: String) {
+        guard !word.isEmpty else { return }
+        var nodeCounter = sentinel
+
+        let characters = word.map{$0}
+        
+        characters
+            .forEach { character in
+                if let existingNode = nodeCounter.children[character] {
+                    nodeCounter = existingNode
+                } else {
+                    nodeCounter.addChild(withValue: character)
+                    nodeCounter = nodeCounter.children[character]!
+                }
+            }
+        nodeCounter.isEnd = true
+    }
+    
+    func insert(words: [String]) {
+        words.forEach(insert)
+    }
+    
+    func longestPrefixWord() -> String {
+        var nodeCounter = sentinel
+        var stringCounter = ""
+
+
+        while nodeCounter.children.count == 1 {
+            let nextNode = nodeCounter.children.first!.value
+            nodeCounter = nextNode
+            
+            stringCounter += String(nextNode.value!)
+        }
+        
+        return stringCounter
+    }
+    
 }
