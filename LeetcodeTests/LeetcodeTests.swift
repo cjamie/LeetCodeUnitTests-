3724a255005a258109a2f46e34e42d8c3a697eac
Jian Ma <jianquanma@gmail.com>
2021-01-03 22:25:03 -0500

    deleeted leetcode test

---
+++ LeetcodeTests/LeetcodeTests.swift
@@ -0,0 +1,16 @@
+//
+//  LeetcodeTests.swift
+//  LeetcodeTests
+//
+//  Created by Jamie Chu on 12/26/20.
+//
+
+import XCTest
+@testable import Leetcode
+
+class LeetcodeTests: XCTestCase {
+
+    func testExample() {
+        //random 
+    }
+}
